d36d98ad288bd293002f7599cc4a229e3fcd9dcf
imsouveek <imsouveek@gmail.com>
2025-06-20 19:31:34 +0530

    Remove unnecessary API call if user is logged in

--- plugins/initSession.client.js
+++ plugins/initSession.client.js
@@ -1,5 +1,6 @@
-export default ({ $api }, inject) => {
+export default ({ $api, store }, inject) => {
     inject('initSession', async () => {
+        if (store.state.auth.isLoggedIn) return
         try {
             await $api.$get('/users')
         } catch (err) {
