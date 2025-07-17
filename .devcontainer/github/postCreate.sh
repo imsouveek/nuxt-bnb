ce8ff5f7fd1e42cdf08508be58b058af03a495a5
imsouveek <imsouveek@gmail.com>
2025-07-17 16:26:24 +0530

    Fixing broken codespace

---
+++ .devcontainer/github/postCreate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Have to call npm install after codespace container is created
+npm install
+
+# Generate prisma client
+npx prisma generate --schema=server/payment/bootstrap/prisma/schema.prisma
+
+npm run dev
