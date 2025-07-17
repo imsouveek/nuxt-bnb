a8d336f20b7560e5c573dcac4622a5b9e8e3d813
imsouveek <imsouveek@gmail.com>
2025-07-17 20:26:02 +0530

    Seed data into Codespace

--- .devcontainer/github/postCreate.sh
+++ .devcontainer/github/postCreate.sh
@@ -6,4 +6,11 @@ npm install
 # Generate prisma client
 npx prisma generate --schema=server/payment/bootstrap/prisma/schema.prisma
 
+# Restore Mongo DB data
+mongorestore -u sobo -p secret --host mongodb --port 27017 --drop db/seed/mongodb/
+
+# Restore Postgres DB data
+psql -h postgresdb -p 5432 -U sobo -d nuxtbnb -f db/seed/postgres/nuxtbnb_seed.sql
+
+# Start dev server
 npm run dev
